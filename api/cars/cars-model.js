dc9600bff39834bbac095445b54504230070cf7b
mgrobstein <97206147+mgrobstein@users.noreply.github.com>
2022-06-03 02:34:17 -0400

    Finished everything

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,24 @@
+const db = require('../../data/db-config')
+
+
 const getAll = () => {
+
+  return db('cars')
   // DO YOUR MAGIC
 }
 
 const getById = () => {
   // DO YOUR MAGIC
+  return db('cars').where('id', id).first()
 }
 
 const create = () => {
   // DO YOUR MAGIC
+}
+
+
+module.exports = {
+  getAll,
+  getById,
+  create
 }
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,33 @@
 // STRETCH
+const cars = [{
+    vin: '11114561111123232',
+    make: 'Toyota',
+    model: 'Prius',
+    mileage: 215000,
+    title: 'clean',
+    transmission: 'manual'
+},
+{
+    vin: '11114561111123233',
+    make: 'Toyota',
+    model: 'Carolla',
+    mileage: 189000,
+    title: 'salvage',
+    transmission: 'manual'
+},
+{
+    vin: '11114561111123234',
+    make: 'Ford',
+    model: 'Focus',
+    mileage: 16000,
+}]
+
+// exports.seed = function(knex){
+//     return knex('cars').truncate().then(()=>{
+//         return knex('cars').insert(cars)
+// })}
+
+exports.seed = async function(knex){
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
